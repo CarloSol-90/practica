8fb218cd65a2f15c351f774268b7772b0833fd4b
Carlos <carlossoler90@gmail.com>
2023-08-26 17:16:59 +0200

    actualización del contenido

--- ficherob.js
+++ ficherob.js
@@ -1,3 +1,3 @@
-console.log("****************");
-console.log("Soy el fichero B");
-console.log("****************");
+console.log("----------------");
+console.log("Soy segundo el fichero");
+console.log("----------------");
